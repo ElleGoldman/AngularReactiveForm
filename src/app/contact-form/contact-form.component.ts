32ea74ac7d2dd858999082887bcbe6e8f7841213
Elena <goldmann.elena@gmail.com>
2022-05-25 22:22:55 +0300

    add custom validation

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, Output, EventEmitter  } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
-import { Validators } from '@angular/forms';
+import { FormBuilder, AbstractControl, Validators, ValidatorFn, ValidationErrors } from '@angular/forms';
 import { Contact } from '../contact';
 
 @Component({
@@ -8,23 +7,37 @@ import { Contact } from '../contact';
   templateUrl: './contact-form.component.html',
   styleUrls: ['./contact-form.component.scss']
 })
+
 export class ContactFormComponent implements OnInit {
   @Output() contactAdded: EventEmitter<Contact> = new EventEmitter();
 
   contactForm = this.formBuilder.group({
-    name: ['', Validators.required],
+    name: ['',  [Validators.required, ValidatePhone()]],
     age: ['', Validators.required],
-    email: ['', Validators.compose([Validators.required, Validators.email])],
+    email: ['', [Validators.required, Validators.email]],
   });
 
   constructor(private formBuilder: FormBuilder) { }
 
+  get name() {
+    return this.contactForm.controls['name'];
+  }
+
   onSubmit() {
+    console.log(this.contactForm.value)
     this.contactAdded.emit(this.contactForm.value)
     this.contactForm.reset();
   }
 
   ngOnInit(): void {
   }
+}
 
+function ValidatePhone(): ValidatorFn  {
+  return (control:AbstractControl) : ValidationErrors | null => {
+    if (control.value && control.value[0].toUpperCase() !== control.value[0]) {
+      return { 'nameInvalid': true };
+    }
+    return null;
+  }
 }
